c555652b7eb174a4cb3011537609c784c26ed917
jchee <jasonchee@live.com>
2017-09-19 15:45:27 -0700

    parsing command line

--- perceptron.py
+++ perceptron.py
@@ -5,6 +5,7 @@ from vector_operations import *
 from math import tanh
 import random
 import sys
+import re
 
 
 def perceptron(x, w, err, theta):
@@ -46,13 +47,15 @@ def get_update_function(name):
     else:
         raise
 
-# def parse_ground_file(ground_file):
-#     with open(ground_file, 'r') as f:
-#         for line in f:
-#             if line.rstrip() == 'NBF':
-#                 fn = line + next(f)
-#
-#     return fn
+
+def parse_ground_file(ground_file):
+    # iterate through the regex and have a lambda for each iteration
+    with open(ground_file, 'r') as f:
+        for line in f:
+            if line.rstrip() == 'NBF':
+                fn = line + next(f)
+
+    return fn
 
 
 def generate_ground_function(ground):
@@ -66,12 +69,18 @@ def generate_training_data():
 def main():
     num_args = len(sys.argv)
     if num_args != 8:
-        print 'INCORRECT PARAMETERS'
-        print sys.argv
-        return
+        print 'Incorrect number of params ' + sys.argv
+        raise SystemExit(1)
+
     print sys.argv[1]
-    # activation = get_activation_function(sys.argv[0])
-    # training_alg = get_update_function(sys.argv[1])
+
+    activation = get_activation_function(sys.argv[1])
+    training_alg = get_update_function(sys.argv[2])
+    ground_file = parse_ground_file(sys.argv[3])
+    distribution = sys.argv[4]
+    num_train = sys.argv[5]
+    num_test = sys.argv[6]
+    epsilon = sys.argv[7]
 
 
 if __name__ == "__main__":
